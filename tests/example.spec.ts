79ba0a7f3d293e719856621974466c05e8fd14f1
Prashant Panchal <prashantp2524@gmail.com>
2026-02-04 23:25:46 +0530

    prash commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,10 @@
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  // await page.goto('https://playwright.dev/');
-
-  // // Expect a title "to contain" a substring.
-  // await expect(page).toHaveTitle(/Playwright/);
   console.log("Hello world");
+});
+test("has title", async ({ page }) => {
+  console.log("Hello world 1");
 });
 
 // test('get started link', async ({ page }) => {
