8bc1d4377b55dc03f820f7faf918378e8f376b48
Prashant Panchal <prashantp2524@gmail.com>
2026-03-21 02:19:31 +0530

    removed only

--- tests/Annotations.spec.ts
+++ tests/Annotations.spec.ts
@@ -4,7 +4,7 @@ test.describe("Annotations", () => {
   test("Test example 1", async ({ page }) => {
     console.log("Test example 1");
   });
-  test.only("Test example 2", async ({ page }) => {
+  test("Test example 2", async ({ page }) => {
     console.log("Test example 2");
   });
   test("Test example 3", async ({ page }) => {
